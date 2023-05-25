dd4a91d338ac8da8fd97c5c0e26884d8309326b4
Deviously3011 <117723597+Deviously3011@users.noreply.github.com>
2023-05-25 14:28:54 +0200

    Add files via upload

---
+++ insert.php
@@ -0,0 +1,75 @@
+<?php
+$host = 'localhost:3307';
+$db   = 'test';
+$user = 'root';
+$pass = '';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+try 
+{
+     $pdo = new PDO($dsn, $user, $pass, $options);
+     echo "Connectie gemaakt!";
+} 
+catch (\PDOException $e) 
+{
+     throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
+
+
+// Verwerken van het formulier
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Ontvang de gegevens van het formulier
+    $product_naam = $_POST["product_naam"];
+    $prijs_per_stuk = $_POST["prijs_per_stuk"];
+    $omschrijving = $_POST["omschrijving"];
+
+    // Query voorbereiden met named parameters
+    $query = 'INSERT INTO producten (product_naam, prijs_per_stuk, omschrijving) VALUES (:product_naam, :prijs_per_stuk, :omschrijving)';
+    $statement = $pdo->prepare($query);
+
+    // Data binden aan de named parameters
+    $data = array(
+        "product_naam" => $product_naam,
+        "prijs_per_stuk" => $prijs_per_stuk,
+        "omschrijving" => $omschrijving
+    );
+
+    // Query uitvoeren
+    $result = $statement->execute($data);
+
+    // Controleer of de query succesvol was
+    if ($result) {
+        echo "Product succesvol toegevoegd.";
+    } else {
+        echo "Er is een fout opgetreden bij het toevoegen van het product.";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Product Toevoegen</title>
+</head>
+<body>
+    <h1>Product Toevoegen</h1>
+    <form method="POST" action="insert.php">
+        <label for="product_naam">Productnaam:</label>
+        <input type="text" name="product_naam" required><br><br>
+        
+        <label for="prijs_per_stuk">Prijs per stuk:</label>
+        <input type="number" name="prijs_per_stuk" required><br><br>
+        
+        <label for="omschrijving">Omschrijving:</label>
+        <textarea name="omschrijving" required></textarea><br><br>
+        
+        <input type="submit" value="Toevoegen">
+    </form>
+</body>
+</html>
